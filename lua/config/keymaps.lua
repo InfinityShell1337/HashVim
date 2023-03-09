a18da61b2b15be6a319c4635b9b5594d17c0d433
Infinity <InfinityShell1337@github.com>
2023-03-08 22:24:07 -0500

    Updated

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set({ "n", "v" }, "_", '"_')
+vim.keymap.set({ "n", "v" }, "+", '"+')
+
+vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>a")
+vim.keymap.set("n", "<C-M-Tab>", "<Esc>:set wrap spell cursorline cursorcolumn<CR>")
+
+vim.keymap.set("n", "<<", "<C-c>")
+vim.keymap.set("i", "<<", "<C-c>")
+vim.keymap.set("v", "<<", "<C-c>")
+vim.keymap.set("c", "<<", "<C-c>")
+vim.keymap.set("t", "<<", "<C-\\><C-n>")
+vim.keymap.set("n", ";", ":")
+vim.keymap.set("v", ";", ":")
+
+vim.api.nvim_create_user_command("Wq", "wq", {})
+vim.api.nvim_create_user_command("Sw", "w !sudo tee %", {})
+vim.api.nvim_create_user_command("T", "term fish", {})
